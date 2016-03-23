55bc5e3d217dd3cb75fa04cb5bae322fd600f678
ValleJulien <j.valle.julien@gmail.com>
2016-03-23 17:16:06 +0100

    init api architecture

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,7 +28,7 @@ Route::get('/autocomplete', function () {
 // get Towns data
 Route::get('/autocomplete/town', 'AutoCompleteController@getTownAutoComplete');
 
-// get Values 
+// get Values
 Route::get('/days/{days}', 'GeometryController@getDays');
 
 // get Towns data
@@ -39,3 +39,33 @@ Route::get('/market-near', 'GeocodeController@getNearestMarket');
 
 // Cron
 Route::get('/cron', 'CronController@updateJson');
+
+/*
+|--------------------------------------------------------------------------
+| Routes uri for API to Ionic
+|--------------------------------------------------------------------------
+|
+| Route::get('/api/v1/products/{id?}', ['middleware' => 'auth.basic', function($id = null) defines a RESTful URL for version 1 of the API
+| The requested resource is Products. {id?} specifies an optional parameter.
+| The id is used to retrieve a single product. The API uses basic authentication
+| and give it the controller to call when that URI is requested.
+|--------------------------------------------------------------------------
+| The routes are calling the respective models to retrieve the data from the database.
+| return Response::json(…) returns the results in JSON format.
+*/
+
+
+
+
+// Route::get('/api/v1/categories/{id?}', ['middleware' => 'auth.basic', function($id = null) {
+// if ($id == null) {
+//     $categories = App\Category::all(array('id', 'name'));
+// } else {
+//     $categories = App\Category::find($id, array('id', 'name'));
+// }
+// return Response::json(array(
+//             'error' => false,
+//             'user' => $categories,
+//             'status_code' => 200
+//         ));
+// }]);
